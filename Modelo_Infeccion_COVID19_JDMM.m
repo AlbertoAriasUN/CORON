56ff2811427b2a76be4567b8978233c1a8b821e2
Julián Medina <juliandm1995@gmail.com>
2020-03-31 16:30:21 -0500

    31/03

--- Modelo_Infeccion_COVID19_JDMM.m
+++ Modelo_Infeccion_COVID19_JDMM.m
@@ -3,7 +3,7 @@ clc;close all;clear all
 diasFuturos = 6; %Cuantos días en el futuro tomará la simulación
 diaInicial = 6; %Este valor solo tiene efecto en el label de la gráfica y no en el modelo. El modelo comienza en día 0. Toma 6 para que sea 6 de marzo.
 
-DatosNInfectados = [1 1 1 3 3 9 13 16 24 45 57 75 93 108 158 210 235 306 378 470 491 539 610 702]; %Vector de datos con el número de infectados diarios desde el 6 de marzo hasta el 29 de marzo
+DatosNInfectados = [1 1 1 3 3 9 13 16 24 45 57 75 93 108 158 210 235 306 378 470 491 539 610 702 798 906]; %Vector de datos con el número de infectados diarios desde el 6 de marzo hasta el 31 de marzo
 
 vectorDias =  0:size(DatosNInfectados,2)-1; %Vector que va desde el día 0 de la infección hasta el día actual
 modeloObjeto = fit(vectorDias',DatosNInfectados','exp1') %Retorna objeto con las constantes del modelo (a y b) 
@@ -36,5 +36,5 @@ nMananaMax = nManana*(1+tolerancia);
 
 disp("Mañana "+ (n+6) +" habrán entre " + round(nMananaMin) + " y " + round(nMananaMax) + " casos")
 
-labels = ["06/03","07/03","08/03","09/03","10/03","11/03","12/03","13/03","14/03","15/03","16/03","17/03","18/03","19/03","20/03","21/03","22/03","23/03","24/03","25/03","26/03","27/03","28/03","29/03","30/03","31/03", "01/04", "02/04", "03/04", "04/04"];
+labels = ["06/03","07/03","08/03","09/03","10/03","11/03","12/03","13/03","14/03","15/03","16/03","17/03","18/03","19/03","20/03","21/03","22/03","23/03","24/03","25/03","26/03","27/03","28/03","29/03","30/03","31/03", "01/04", "02/04", "03/04", "04/04", "05/04", "06/04"];
 generarJSON(DatosNInfectados,round(modelo),labels);
